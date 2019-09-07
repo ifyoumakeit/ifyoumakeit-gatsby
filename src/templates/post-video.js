94d0b6d333aa9c932e2563d399e3ef89eb198a16
Dave Garwacke <admin+git@ifyoumakeit.com>
2019-09-07 18:01:05 -0400

    Move video to top
    The video is the main thing people care about.
    Not my terrible writing.

--- src/templates/post-video.js
+++ src/templates/post-video.js
@@ -2,6 +2,7 @@ import React from "react";
 import { graphql } from "gatsby";
 
 import Layout from "../components/layout";
+import styles from "./post-video.module.css";
 
 export const postListQuery = graphql`
   query($id: String!) {
@@ -35,26 +36,29 @@ const IndexPage = ({ data, ...props }) => {
 
   return (
     <Layout>
-      <strong>{post.category.title}</strong>
+      {postmeta.video_file && (
+        <div className={styles.container}>
+          <iframe
+            className={styles.iframe}
+            title="player"
+            type="text/html"
+            width="640"
+            height="360"
+            src={
+              postmeta.video_server === "y"
+                ? `https://www.youtube.com/embed/${postmeta.video_file}`
+                : `https://player.vimeo.com/video/${postmeta.video_file}?title=0&byline=0&portrait=0`
+            }
+            frameBorder="0"
+            allowFullScreen
+          />
+        </div>
+      )}
+
       <h1
         dangerouslySetInnerHTML={{ __html: `${post.title} - ${post.subtitle}` }}
       />
       <p dangerouslySetInnerHTML={{ __html: post.body }} />
-      {postmeta.video_file && (
-        <iframe
-          title="player"
-          type="text/html"
-          width="640"
-          height="360"
-          src={
-            postmeta.video_server === "y"
-              ? `https://www.youtube.com/embed/${postmeta.video_file}`
-              : `https://player.vimeo.com/video/${postmeta.video_file}?title=0&byline=0&portrait=0`
-          }
-          frameBorder="0"
-          allowFullScreen
-        />
-      )}
     </Layout>
   );
 };
