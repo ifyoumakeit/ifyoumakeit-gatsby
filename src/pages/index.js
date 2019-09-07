58ac8e4613ecc11c22eb6b2e42fbd9e932891d0b
Dave Garwacke <admin+git@ifyoumakeit.com>
2019-09-07 12:39:05 -0400

    Add images to homepage
    Use grid to lay them out

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,24 @@
 import React from "react";
+import Img from "gatsby-image";
 import { graphql } from "gatsby";
 
 import SEO from "../components/seo";
 import Layout from "../components/layout";
-
+import styles from "./index.module.css";
+import getCategorySlug from "../../get-category-post-slug.js";
 export const postListQuery = graphql`
   query {
+    allFile(filter: { name: { glob: "*square*" } }) {
+      nodes {
+        relativePath
+        publicURL
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
     allMysqlCategory(filter: { cat_publish: { eq: 1 } }) {
       edges {
         node {
@@ -26,26 +39,44 @@ export const postListQuery = graphql`
 `;
 
 const IndexPage = ({ data }) => {
-  console.log(data);
+  const images = data.allFile.nodes;
   return (
     <Layout>
       <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
       <h1>Hi people</h1>
       <p>Welcome to If You Make It - Gatsby style.</p>
       <p>Now go build something great.</p>
-
-      {data.allMysqlCategory.edges.map(({ node: category }) => (
-        <div>
-          <h1>{category.cat_title}</h1>
-          <div>
-            {category.posts.slice(0, 2).map(post => (
-              <a href={`${post.title_slug}/${post.subtitle_slug}`}>
-                {post.title} - {post.subtitle}
-              </a>
-            ))}
+      <div className={styles.categories}>
+        {data.allMysqlCategory.edges.map(({ node: category }) => (
+          <div className={styles.category}>
+            <h1>{category.cat_title}</h1>
+            <div>
+              {category.posts.slice(0, 2).map(post => {
+                const image = images.find(
+                  image =>
+                    image.relativePath.indexOf(
+                      `${post.title_slug}-${post.subtitle_slug}`,
+                    ) > -1,
+                );
+                return (
+                  <a
+                    href={`${getCategorySlug(category)}/${post.title_slug}/${
+                      post.subtitle_slug
+                    }`}
+                  >
+                    {image && <Img fluid={image.childImageSharp.fluid} />}
+                    <span
+                      dangerouslySetInnerHTML={{
+                        __html: `${post.title} - ${post.subtitle}`,
+                      }}
+                    />
+                  </a>
+                );
+              })}
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </Layout>
   );
 };
